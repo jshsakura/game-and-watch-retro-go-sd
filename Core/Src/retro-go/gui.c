6d17b5fdfb69e4f193bebc877c5fc3a7df10c8ca
Sylverb <sylver.bruneau@gmail.com>
2026-07-17 17:42:57 +0200

    Retro-go : only save current tab if in launcher as it's not valid in emulators

--- Core/Src/retro-go/gui.c
+++ Core/Src/retro-go/gui.c
@@ -18,6 +18,7 @@
 #include "favorites.h"
 #include "gw_malloc.h"
 #include "rg_system_grid.h"
+#include "appid.h"
 
 #if !defined(COVERFLOW)
 #define COVERFLOW 0
@@ -323,7 +324,15 @@ tab_t *gui_set_current_tab(int index)
 
 void gui_save_current_tab()
 {
+    /* tab->arg points into AHB emulators[], discarded by ahb_init() when a
+     * core starts. Re-saving from inside an emulator (sleep path) would
+     * persist crushed browse_subpath. Values were already committed at launch. */
+    if (odroid_system_get_app()->id != APPID_LAUNCHER)
+        return;
+
     tab_t *tab = gui_get_current_tab();
+    if (!tab)
+        return;
 
     odroid_settings_MainMenuCursor_set(tab->listbox.cursor);
     odroid_settings_MainMenuSelectedTab_set(gui.selected);
